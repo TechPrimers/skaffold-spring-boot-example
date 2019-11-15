5a5b0923febbae1b3b2e18bc518a2e5d178ae994
MovingToWeb <soleil.fils.88@gmail.com>
2019-11-16 00:50:53 +0530

    initial commit - skaffold example

---
+++ src/main/java/com/techprimers/skaffoldspringbootexample/SkaffoldSpringBootExampleApplication.java
@@ -0,0 +1,13 @@
+package com.techprimers.skaffoldspringbootexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkaffoldSpringBootExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkaffoldSpringBootExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techprimers/skaffoldspringbootexample/SkaffoldSpringBootExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techprimers.skaffoldspringbootexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkaffoldSpringBootExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
